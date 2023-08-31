0e3d6a8ad733d1eb80b93832f0c31e69dece2368
Genesses fernandes <102777715+Genesses12@users.noreply.github.com>
2023-08-31 19:44:24 -0400

    salvando alterações

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
+  @Input()
+  Foto_img: string ="https://i.ytimg.com/vi/CtDknAaW2dM/sddefault.jpg";
+  @Input()
+  contentTitle:string="Comtemplem o Mago";
+  @Input()
+  contentDescricao:string  = "O Mago mais podero que já existiu no mundo"
 
 }
